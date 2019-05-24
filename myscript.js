5db020357b3c6187293beadad9c2ff091a45a1ee
zcoder83 <dusrksl@gmail.com>
2019-05-24 23:02:15 +0900

    2019.05.24 version 1.0

---
+++ myscript.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+
+$('#lightSlider').lightSlider({
+    gallery: true,
+    item: 1,
+    loop:true,
+    slideMargin: 0,
+    thumbItem: 9
+});
